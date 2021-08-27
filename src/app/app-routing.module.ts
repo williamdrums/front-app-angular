425157e47ca93ebee55419040b10e9f230662281
William Nascimento <william.nascimento@capgemini.com>
2021-08-27 17:01:00 -0300

    finalizando metodo de exclusão de produto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductCrudComponent } from './views/home/product-crud/product-crud.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
@@ -23,6 +24,10 @@ const routes: Routes = [
   {
     path: "products/update/:id",
     component: ProductUpdateComponent
+  },
+  {
+  path: "products/delete/:id",
+  component: ProductDeleteComponent
   }
 ];
 
--- src/app/components/product/product-delete/product-delete.component.ts
+++ src/app/components/product/product-delete/product-delete.component.ts
@@ -1,24 +1,24 @@
 import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
-  selector: 'app-product-update',
-  templateUrl: './product-update.component.html',
-  styleUrls: ['./product-update.component.css']
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
 })
-export class ProductUpdateComponent implements OnInit {
-
+export class ProductDeleteComponent implements OnInit {
   product: Product = {
     name: '',
     price: null
   };
+
   constructor(
     private productService: ProductService,
-    private router: Router,
-    private route: ActivatedRoute
-  ) { }
+     private router: Router,
+     private route: ActivatedRoute
+     ) { }
 
   ngOnInit(): void {
     const id = parseInt(this.route.snapshot.paramMap.get('id')!);
@@ -27,15 +27,14 @@ export class ProductUpdateComponent implements OnInit {
     });
   }
 
-  updateProduct(): void {
-
-    this.productService.update(this.product).subscribe(() => {
-      this.productService.showMessage('Product updated successfully')
+  deleteProduct(): void {
+    this.productService.delete(this.product.id!).subscribe(() => {
+      this.productService.showMessage('Successfully deleted product!');
       this.router.navigate(['/products']);
     });
   }
 
   cancel(): void {
-    this.router.navigate(['/products']);
+    this.router.navigate(['/products'])
   }
 }
